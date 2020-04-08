69fd3fe43d56806f8608e34562955d2a0b90c164
killer99 <372688974@qq.com>
2020-04-08 09:36:04 +0800

    xg

---
+++ src/lang/en-expressage.js
@@ -0,0 +1,7 @@
+﻿export default {
+  noexpress: 'No need for logistics',
+  shunfeng: 'SF express',
+  shentong: 'Shentong express',
+  yuantong: 'YTO Express',
+  yunda: 'Yunda Express'
+}
---
+++ src/lang/en-finance.js
@@ -0,0 +1,26 @@
+﻿// 财务管理
+export default {
+  balance: 'Balance',
+  balance1: 'Withdrawal balance',
+  totalIncome: 'Cumulative income',
+  toBeIncome: 'Income to be determined',
+  tixianing: 'Withdrawal in progress',
+  tixianed: 'Cash withdrawn',
+  tixian: 'Account withdrawal',
+  change: 'Amount change',
+  payType: 'Payment type',
+  type: 'Operation type',
+  type2: 'User order',
+  type3: 'User return',
+  type4: 'Merchants withdraw cash',
+  type5: 'Red envelope settings',
+  type6: 'Red envelope return',
+  type7: 'Cash withdrawal return',
+  genTime: 'Time',
+  param: 'Relation',
+  tixianMoney: 'Withdrawal amount',
+  txStatus: 'Status',
+  txStatus1: 'To be paid',
+  txStatus2: 'Paid',
+  txStatus3: 'Canceled'
+}
